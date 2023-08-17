36585d4f228e092bcb9b8b3fa2ff4527f1ac4c9f
linhpn3 <linhpn3@smartosc.com>
2023-08-17 10:22:37 +0700

    show page number

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,6 +1,12 @@
-import Link from "next/link";
-
 const Pagination = ({ currentPage, pages, switchPage }) => {
+	const getPageNumbers = () => {
+		const pageNumbers = [];
+		for (let i = 1; i <= pages; i++) {
+			pageNumbers.push(i);
+		}
+		return pageNumbers;
+	};
+
 	return (
 		<div className="flex justify-between items-center mt-8">
 			<button
@@ -10,9 +16,24 @@ const Pagination = ({ currentPage, pages, switchPage }) => {
 			>
 				Previous Page
 			</button>
-			<p className="text-xl text-slate-900 tracking-wide dark:text-slate-200">
+			{/* <p className="text-xl text-slate-900 tracking-wide dark:text-slate-200">
 				Page {currentPage} of {pages}
-			</p>
+			</p> */}
+			<div className="flex items-center space-x-2">
+				{getPageNumbers().map((pageNumber) => (
+					<button
+						key={pageNumber}
+						onClick={() => switchPage(pageNumber)}
+						className={`${
+							pageNumber === currentPage
+								? "bg-blue-500 text-white"
+								: "bg-gray-200 text-gray-700 hover:bg-gray-300"
+						} rounded-full w-8 h-8 flex items-center justify-center leading-none font-semibold transition duration-300 ease-in-out`}
+					>
+						{pageNumber}
+					</button>
+				))}
+			</div>
 			<button
 				onClick={() => switchPage(currentPage + 1)}
 				disabled={currentPage === pages}
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,35 +2,44 @@ import { useEffect, useState } from "react";
 import api from "@/services/backendApi";
 import PostList from "@/components/PostListClient";
 import ClientLayout from "@/components/layouts/ClientLayout";
+import Pagination from "@/components/Pagination";
 
 const Index = () => {
 	const [posts, setPosts] = useState([]);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [pages, setPages] = useState(1);
 	const [searchTerm, setSearchTerm] = useState("");
+	const [categories, setCategories] = useState([]);
+	const [selectedCategory, setSelectedCategory] = useState("");
 
 	const fetchData = async (page = 1) => {
 		const response = await api.get(
-			`/api/posts?page=${page}&title=${searchTerm}`
+			`/api/posts?page=${page}&title=${searchTerm}&category=${selectedCategory}`
 		);
 		setCurrentPage(page);
-		setPages(Math.ceil(response.data.count / 10));
+		setPages(Math.ceil(response.data.count / 5));
 		setPosts(response.data.posts);
 	};
 
 	useEffect(() => {
-		const fetchData = async () => {
+		const fetchPosts = async () => {
 			const response = await api.get("/api/posts");
-
-			console.log(response);
-
 			if (response.status === 200) {
 				setPages(Math.ceil(response.data.count / 5));
 				setPosts(response.data?.posts);
 			}
 		};
 
-		fetchData();
+		const fetchCategories = async () => {
+			const response = await api.get("/api/admin/categories");
+			console.log({ cate: response });
+			if (response.status === 200) {
+				setCategories(response.data.categories);
+			}
+		};
+
+		fetchPosts();
+		fetchCategories();
 	}, []);
 
 	const handleSearch = (event) => {
@@ -40,10 +49,24 @@ const Index = () => {
 
 	return (
 		<ClientLayout>
-			<form onSubmit={handleSearch} className="mb-8">
+			<form onSubmit={handleSearch} className="mb-8 flex items-center">
 				<label htmlFor="search" className="sr-only">
 					Search
 				</label>
+				<select
+					value={selectedCategory}
+					onChange={(event) =>
+						setSelectedCategory(event.target.value)
+					}
+					className="bg-gray-100 border-gray-400 border-2 py-2 px-4 mr-4 rounded-lg"
+				>
+					<option value="">All Categories</option>
+					{categories.map((category) => (
+						<option key={category.id} value={category.id}>
+							{category.title}
+						</option>
+					))}
+				</select>
 				<input
 					type="text"
 					id="search"
@@ -51,30 +74,23 @@ const Index = () => {
 					value={searchTerm}
 					onChange={(event) => setSearchTerm(event.target.value)}
 					placeholder="Search by title"
-					className="bg-gray-100 border-gray-400 border-2 py-2 px-4 rounded-lg w-full"
+					className="bg-gray-100 border-gray-400 border-2 py-2 px-4 rounded-lg w-full mr-4"
 				/>
+
+				<button
+					type="submit"
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+				>
+					Search
+				</button>
 			</form>
-			<h2 className="text-xl font-semibold mb-2">All Posts</h2>
+			<h2 className="text-2xl font-semibold mb-4">All Posts</h2>
 			<PostList posts={posts} />
-			<div className="flex justify-between items-center mt-8">
-				<button
-					onClick={() => fetchData(currentPage - 1)}
-					disabled={currentPage === 1}
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-				>
-					Previous Page
-				</button>
-				<p className="text-xs text-slate-900 tracking-tight dark:text-slate-200">
-					Page {currentPage} of {pages}
-				</p>
-				<button
-					onClick={() => fetchData(currentPage + 1)}
-					disabled={currentPage === pages}
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-				>
-					Next Page
-				</button>
-			</div>
+			<Pagination
+				currentPage={currentPage}
+				pages={pages}
+				switchPage={fetchData}
+			/>
 		</ClientLayout>
 	);
 };
