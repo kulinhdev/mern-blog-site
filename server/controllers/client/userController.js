7d0a1ab19094760cdf04973a6223032849c72021
linhpn3 <linhpn3@smartosc.com>
2023-09-18 16:31:15 +0700

    api update user password

--- server/controllers/client/userController.js
+++ server/controllers/client/userController.js
@@ -1,5 +1,9 @@
+const bcrypt = require("bcryptjs");
+const express = require("express");
+
 const User = require("../../models/User");
 
+const router = express.Router();
 async function updateProfile(req, res) {
 	const { userId, userName, firstName, lastName, avatar } = req.body;
 
@@ -40,4 +44,45 @@ async function updateProfile(req, res) {
 	}
 }
 
-module.exports = { updateProfile };
+async function updatePassword(req, res) {
+	const { userId, currentPassword, newPassword } = req.body;
+
+	try {
+		// Find the user by ID
+		const user = await User.findById(userId);
+
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		// Check if the current password provided matches the stored hashed password
+		const isPasswordCorrect = await bcrypt.compare(
+			currentPassword,
+			user.password
+		);
+
+		if (!isPasswordCorrect) {
+			return res
+				.status(401)
+				.json({ message: "Current password is incorrect" });
+		}
+
+		// Hash the new password
+		const hashedNewPassword = await bcrypt.hash(newPassword, 12);
+
+		// Update the user's password
+		user.password = hashedNewPassword;
+
+		// Save the updated user profile
+		await user.save();
+
+		return res
+			.status(200)
+			.json({ message: "Password updated successfully" });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: error.message });
+	}
+}
+
+module.exports = { updateProfile, updatePassword };
--- server/routes/api/client/user.Routes.js
+++ server/routes/api/client/user.Routes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.put("/update-profile", userController.updateProfile);
 
+router.put("/update-password", userController.updatePassword);
+
 module.exports = router;
